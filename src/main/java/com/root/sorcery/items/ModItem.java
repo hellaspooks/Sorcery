0ebc6a552fb689ff4183357b5737290a29088d0c
hellaspooks <50092809+hellaspooks@users.noreply.github.com>
2019-09-20 12:45:38 -0700

    Add files via upload

--- src/main/java/com/root/sorcery/items/ModItem.java
+++ src/main/java/com/root/sorcery/items/ModItem.java
@@ -18,12 +18,26 @@ public class ModItem extends Item {
     public static ModItem chondrite_chunk;
     public static ModItem chondrite_ingot;
     public static ModItem siderite_ingot;
+    public static ModItem sigil_slate;
+    public static ModItem sigil_evocation;
+    public static ModItem sigil_conjuration;
+    public static ModItem sigil_abjuration;
+    public static ModItem sigil_enchantment;
+    public static ModItem sigil_necromancy;
+    public static ModItem sigil_transmutation;
 
     public static void init(){
         lodestone = new ModItem( "lodestone");
         chondrite_chunk = new ModItem( "chondrite_chunk");
         chondrite_ingot = new ModItem("chondrite_ingot");
         siderite_ingot = new ModItem("siderite_ingot");
+        sigil_slate = new ModItem("sigil_slate");
+        sigil_evocation = new ModItem("sigil_evocation");
+        sigil_conjuration = new ModItem("sigil_conjuration");
+        sigil_abjuration = new ModItem("sigil_abjuration");
+        sigil_enchantment = new ModItem("sigil_enchantment");
+        sigil_necromancy = new ModItem("sigil_necromancy");
+        sigil_transmutation = new ModItem("sigil_transmutation");
         
         Geode.init();
         Crystals.init();
