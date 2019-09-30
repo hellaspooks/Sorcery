cac3f4a6882176ff58bff2ddd4678680a97b4dec
Ourten <ourtencoop@gmail.com>
2019-09-30 22:22:43 +0200

    Add updated Reliquary model

--- src/main/java/com/root/sorcery/client/model/Models.java
+++ src/main/java/com/root/sorcery/client/model/Models.java
@@ -1,57 +1,50 @@
 package com.root.sorcery.client.model;
 
-import com.google.common.collect.ImmutableMap;
+import com.root.sorcery.client.LichMod;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.IUnbakedModel;
 import net.minecraft.client.renderer.model.ModelResourceLocation;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelBakeEvent;
-import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.client.model.BasicState;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
-import net.minecraftforge.client.model.obj.OBJModel;
 import net.minecraftforge.common.model.TRSRTransformation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import javax.vecmath.Quat4f;
 import javax.vecmath.Vector3f;
+import java.util.Random;
 
 import static com.root.sorcery.Constants.MODID;
-import static net.minecraft.util.Direction.Plane.HORIZONTAL;
 
 @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Models
 {
+    private static final Random RAND          = new Random();
+    private static final Quat4f LICH_ROTATION = new Quat4f(RAND.nextFloat(), RAND.nextFloat(), RAND.nextFloat(), RAND.nextFloat());
+
     @SubscribeEvent
     public static void onModelBakeEvent(ModelBakeEvent event)
     {
-        IUnbakedModel model = ModelLoaderRegistry.getModelOrMissing(new ResourceLocation(MODID + ":block/reliquary" +
-                ".obj")).process(ImmutableMap.of("flip-v", "true"));
+        IUnbakedModel model = ModelLoaderRegistry.getModelOrMissing(new ResourceLocation(MODID + ":block/reliquary"));
 
         TRSRTransformation modelTRSRT = ((TRSRTransformation) model.getDefaultState()).compose(new TRSRTransformation(
-                new Vector3f(0.5f, 0.5f, 0.5f),
+                new Vector3f(0, 1, 0),
+                LichMod.isLichModActivated() ? LICH_ROTATION : null,
                 null,
-                new Vector3f(0.0625F, 0.0625F, 0.0625F),
                 null));
 
-        if (model instanceof OBJModel)
+        Direction.Plane.HORIZONTAL.iterator().forEachRemaining(direction ->
         {
-            HORIZONTAL.iterator().forEachRemaining(direction ->
-            {
-                IBakedModel bakedModel = model.bake(event.getModelLoader(), ModelLoader.defaultTextureGetter(),
-                        new BasicState(modelTRSRT.compose(TRSRTransformation.from(direction)), false),
-                        DefaultVertexFormats.BLOCK);
-                event.getModelRegistry().put(new ModelResourceLocation(MODID + ":reliquary",
-                        "facing=" + direction.getName2()), bakedModel);
-            });
-        }
-    }
-
-    @SubscribeEvent
-    public static void onPreTextureStitch(TextureStitchEvent.Pre event)
-    {
-        event.addSprite(ResourceLocation.tryCreate(MODID + ":block/reliquary"));
+            IBakedModel bakedModel = model.bake(event.getModelLoader(), ModelLoader.defaultTextureGetter(),
+                    new BasicState(modelTRSRT.compose(TRSRTransformation.from(direction)), false),
+                    DefaultVertexFormats.BLOCK);
+            event.getModelRegistry().put(new ModelResourceLocation(MODID + ":reliquary",
+                    "facing=" + direction.getName2()), bakedModel);
+        });
     }
 }
