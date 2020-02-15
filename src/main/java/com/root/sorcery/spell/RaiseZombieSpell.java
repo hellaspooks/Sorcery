c4b1bdd49c7536f85281af3fc47909f0aef93913
bucksnort2 <smi14048@byui.edu>
2020-02-15 11:59:27 -0700

    added particle effects (stolen from BlinkSpell.java, will update with custom particles)

--- src/main/java/com/root/sorcery/spell/RaiseZombieSpell.java
+++ src/main/java/com/root/sorcery/spell/RaiseZombieSpell.java
@@ -1,4 +1,35 @@
 package com.root.sorcery.spell;
 
-public class RaiseZombieSpell {
+        import com.root.sorcery.Config;
+        import com.root.sorcery.network.PacketHandler;
+        import com.root.sorcery.network.packets.ParticleEffectPacket;
+        import com.root.sorcery.particle.ParticleEffects;
+        import net.minecraft.entity.player.PlayerEntity;
+        import net.minecraft.util.ActionResultType;
+        import net.minecraft.util.math.Vec3d;
+
+public class RaiseZombieSpell extends DurationSpell
+{
+    public RaiseZombieSpell()
+    {
+
+    }
+
+    @Override
+    public ActionResultType castServer(SpellUseContext context) {
+
+        PlayerEntity player = context.getPlayer();
+
+        return ActionResultType.SUCCESS;
+    }
+
+    @Override
+    public void doParticleEffects(SpellUseContext context)
+    {
+        Vec3d loc = context.getPlayer().getPositionVec().add(0,1, 0);
+        Vec3d look = context.getPlayer().getLookVec();
+
+        ParticleEffectPacket pkt = new ParticleEffectPacket(2, ParticleEffects.getSpark(), loc, look, 100, 0.5, 0.2);
+        PacketHandler.sendToAllTracking(context.getPlayer(), pkt);
+    }
 }
